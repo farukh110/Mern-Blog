679f5425444f60a562593c35b0ff4b46923a5038
farukh110 <farukhsajjad110@gmail.com>
2023-06-16 23:44:22 +0500

    created different pages

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,18 @@ const { PORT } = require('./config/index');
 const router = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+const corsOptions = {
+    credentials: true,
+    origin: ['http://127.0.0.1:5173']
+}
 
 const app = express();
 
 app.use(cookieParser());
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
